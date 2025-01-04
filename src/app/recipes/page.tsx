1477f00aa397b518c18222fb4b2fdf3c2e0f645e
0herm <0herm@protonmail.com>
2025-01-04 15:59:57 +0100

    remove unused

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -13,7 +13,6 @@ import {
     CardTitle
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Button } from '@/components/ui/button'
 
 export default function RecipesPage(){
 
