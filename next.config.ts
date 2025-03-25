4c4067e6a55ee199e5927f70fc6fa0f6b652919d
0herm <0herm@protonmail.com>
2025-03-25 12:01:19 +0100

    added version to footer

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    env: {
+        NEXT_PUBLIC_VERSION: process.env.npm_package_version,
+    },
     images: {
         remotePatterns: [
         {
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,11 @@
 export default function Footer() {
 
-	return (
-		<div className="flex flex-row items-center w-full p-4">
-            <p className="text-sm">Opphavsrett © {new Date().getFullYear()} Herbivorene</p>			
-		</div>
-	)
+    const version = process.env.NEXT_PUBLIC_VERSION
+
+    return (
+        <div className="flex flex-row items-center w-full p-4">
+            <p className="text-sm">Opphavsrett © {new Date().getFullYear()} Herbivorene</p>
+            <p className="ml-auto bg-primary-foreground p-[0.5rem_1rem] rounded-md font-semibold">v{version}</p>
+        </div>
+    )
 }
